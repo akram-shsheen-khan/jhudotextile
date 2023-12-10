c0a85cd50a86a98ffbdb4ca267c729b202b6c287
akram-shsheen-khan <shaheenakram754@gmail.com>
2023-12-11 04:46:10 +0500

    shaheen20

--- src/app/dropdown/partyname/page.tsx
+++ src/app/dropdown/partyname/page.tsx
@@ -98,93 +98,83 @@ const Page = () => {
   }, []);
   return (
     <div>
-      <body className="antialiased bg-gray-200 text-gray-900 font-sans">
-        <div className="flex items-start">
-          <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-            <span className="block w-full text-2xl text-red-800 text-center uppercase font-bold mb-4">
-              Party Name Form
-            </span>
-            <form className="mb-4">
-              <div className="mb-0 md:w-full">
-                <label className="block text-xl text-green-800 font-semibold mb-1">
-                  Party name
-                </label>
-                <input
-                  className="w-full border rounded p-2 outline-none focus:shadow-outline"
-                  type="text"
-                  name="partyname"
-                  id="partyname"
-                  placeholder="Party Name"
-                  value={partyname}
-                  onChange={(e) => {
-                    setPartyName(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="mb-6 md:w-full">
-                <label className="block text-xl text-green-800 font-semibold mb-1">
-                  Code
-                </label>
-                <input
-                  className="w-full border rounded p-2 outline-none focus:shadow-outline"
-                  type="number"
-                  name="code"
-                  id="code"
-                  onFocus={handleFocus}
-                  placeholder="Code"
-                  value={code}
-                  onChange={(e) => {
-                    setCode(Number(e.target.value));
-                  }}
-                />
-              </div>
-              <div className="mb-6 md:w-full">
-                <label className="block text-xl text-green-800 font-semibold mb-1">
-                  Description
-                </label>
-                <input
-                  className="w-full border rounded p-2 outline-none focus:shadow-outline"
-                  type="text"
-                  name="description"
-                  id="description"
-                  placeholder="Description"
-                  value={description}
-                  onChange={(e) => {
-                    setDescription(e.target.value);
-                  }}
-                />
-              </div>
-              <button
-                onClick={onFinish}
-                type="button"
-                className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
+      <div className="flex items-start">
+        <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+          <span className="block w-full text-2xl text-red-800 text-center uppercase font-bold mb-4">
+            Party Name Form
+          </span>
+          <form className="mb-4">
+            <div className="mb-0 md:w-full">
+              <label className="block text-xl text-green-800 font-semibold mb-1">
+                Party name
+              </label>
+              <input
+                className="w-full border rounded p-2 outline-none focus:shadow-outline"
+                type="text"
+                name="partyname"
+                id="partyname"
+                placeholder="Party Name"
+                value={partyname}
+                onChange={(e) => {
+                  setPartyName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-6 md:w-full">
+              <label className="block text-xl text-green-800 font-semibold mb-1">
+                Code
+              </label>
+              <input
+                className="w-full border rounded p-2 outline-none focus:shadow-outline"
+                type="number"
+                name="code"
+                id="code"
+                onFocus={handleFocus}
+                placeholder="Code"
+                value={code}
+                onChange={(e) => {
+                  setCode(Number(e.target.value));
+                }}
+              />
+            </div>
+            <div className="mb-6 md:w-full">
+              <label className="block text-xl text-green-800 font-semibold mb-1">
+                Description
+              </label>
+              <input
+                className="w-full border rounded p-2 outline-none focus:shadow-outline"
+                type="text"
+                name="description"
+                id="description"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+            </div>
+            <button
+              onClick={onFinish}
+              type="button"
+              className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded"
+            >
+              Submit
+            </button>
+          </form>
         </div>
+      </div>
 
-        <div className="grid2">
-          <div className="grid-container2">
-            <table>
-              <thead>
-                <tr className="header2">
-                  <th>
-                    <div>Party Name</div>
-                  </th>
-                  <th>
-                    <div>Code</div>
-                  </th>
-                  <th>
-                    <div>Description</div>
-                  </th>
-                  <th>
-                    <div>Edit</div>
-                  </th>
-                  <th>
-                    <div>Delete</div>
-                  </th>
+      <div className="h-full w-9/12 bg-slate-400 shadow-2xl mx-auto">
+        <div className="h-60 w-full grid gap-2 p-2 grid-cols-2 grid-rows-2">
+          <div className="col-span-2 row-span-2 rounded-xl bg-white overflow-auto">
+            <table className="table-auto w-full">
+              <thead className="sticky top-0 bg-gray-700 text-white">
+                <tr>
+                  <th className="bg-red-700 text-white py-4">Chemical Name</th>
+                  <th className="bg-red-700 text-white py-4">Code</th>
+                  <th className="bg-red-700 text-white py-4">Description</th>
+                  <th className="bg-red-700 text-white py-4">Edit</th>
+                  <th className="bg-red-700 text-white py-4">Delete</th>
                 </tr>
               </thead>
               <tbody>
@@ -205,8 +195,9 @@ const Page = () => {
             </table>
           </div>
         </div>
-      </body>
+      </div>
     </div>
   );
 };
+
 export default withAuth(Page);
