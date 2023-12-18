766a7603b69a96ec14557e300144de962bf12f00
akram-shsheen-khan <shaheenakram754@gmail.com>
2023-12-18 11:55:21 +0500

    akram27

---
+++ src/app/ProductionReports/PartyNameColor/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import withAuth from "@/utils/withAuth";
+import React from "react";
+
+const Page = () => {
+  return <div>Pary Name And Color Report</div>;
+};
+
+export default withAuth(Page);
